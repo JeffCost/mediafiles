d1900869359764e55646fa7ce4bd1851c4f65d19
JeffCost <jefersonc@gmail.com>
2013-06-23 21:17:04 -0500

    Adds method to update the files order

--- controllers/backend/ajax.php
+++ controllers/backend/ajax.php
@@ -6,4 +6,20 @@ class Mediafiles_Backend_Ajax_Controller extends Admin_Controller {
     {
         parent::__construct();
     }
+
+    public function put_sort()
+    {
+        $post_data = Input::get('order');
+        
+        if(isset($post_data) and !empty($post_data))
+        {
+            $order_items = explode(',', $post_data);
+            foreach ($order_items as $order_position => $slug)
+            {
+                $affected = Mediafiles\Model\File::where_id($slug)
+                               ->update(array('order' => $order_position));
+            }
+            return;
+        }
+    }
 }
