dbbb5efc1bc76f94fa6f161cb431c3fe1907fdc2
JeffCost <jefersonc@gmail.com>
2013-06-23 21:19:44 -0500

    Includes ajax file for backend ajax routes

--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'routes/backend/mediafiles.php';
+require_once 'routes/backend/ajax.php';
 
-require_once 'routes/frontend/ajax.php';
 require_once 'routes/frontend/mediafiles.php';
+require_once 'routes/frontend/ajax.php';
