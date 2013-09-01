bcff2b3e87eaadc4e20f5e38f9efeee9dfd78d5b
JeffCost <jefersonc@gmail.com>
2013-09-01 15:22:23 -0500

    Updates settings file to use the pulic media folder

--- config/settings.php
+++ config/settings.php
@@ -2,8 +2,8 @@
 
 return array(
 
-    'image_url'  => 'bundles/mediafiles/images',
-    'image_path' => path('public').'bundles'.DS.'mediafiles'.DS.'images',
-    'file_url'  => 'bundles/mediafiles/files',
-    'file_path' => path('public').'bundles'.DS.'mediafiles'.DS.'files',
+    'image_url'  => 'media/mediafiles/images',
+    'image_path' => path('public').'media'.DS.'mediafiles'.DS.'images',
+    'file_url'  => 'media/mediafiles/files',
+    'file_path' => path('public').'media'.DS.'mediafiles'.DS.'files',
 );
